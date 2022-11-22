7b0fc191a895772e1c2b94d4f52720770b91bcd5
jaehyunlee <jaehyunlee25@gmail.com>
2022-11-22 17:59:29 +0900

    renew

--- test.js
+++ test.js
@@ -1,28 +1,29 @@
 function mneCallDetail(arrDate) {
   const fCall = { post, get };
   const [date, sign, gb] = arrDate;
-  const addr = "/adm/cop/adb/select.do";
+  const addr = "/w/reservation/golfResv/service/ajax_booking_public_times.asp";
   const method = "post";
   const param = {
-    match_day: date.datify(),
-    gubun: "1",
+    date,
   };
   const dictCourse = {
-    11: "단일",
+    44: "단일",
   };
 
   fCall[method](addr, param, {}, (data) => {
     const json = data.jp();
-    const els = json.res;
+    const els = json.rows;
     Array.from(els).forEach((el) => {
-      let { matchDay: date, matchTime: time, price: fee } = el;
-      date = date.rm("-");
-      time = time.rm(":");
-      course = dictCourse[11];
-      const hole = 9;
-      fee = fee.rm(",") * 1;
-      fee_normal = fee;
-      fee_discount = fee;
+      let {
+        TRBM_COURSE: course,
+        TRBM_DATE: date,
+        TRBM_HOLE: hole,
+        TRBM_TIME: time,
+      } = el;
+      course = dictCourse[course];
+      const fee = "50000";
+      fee_normal = fee.rm(",") * 1;
+      fee_discount = fee.rm(",") * 1;
 
       golf_schedule.push({
         golf_club_id: clubId,
